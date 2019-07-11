e966035431cd392ef0fadcbb880968a49d1faa98
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2019-07-11 16:14:27 -0300

    Operação de instanciação

--- src/main/java/com/fatec/modelagem/ModelagemApplication.java
+++ src/main/java/com/fatec/modelagem/ModelagemApplication.java
@@ -1,13 +1,29 @@
 package com.fatec.modelagem;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ModelagemApplication {
+import com.fatec.modelagem.domain.Categoria;
+import com.fatec.modelagem.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ModelagemApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriarepository;
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		categoriarepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
