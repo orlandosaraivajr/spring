635a7e25d9d97e3e4bac80d3386d5c2aaf5cedfa
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2019-07-11 09:40:56 -0300

    Projeto criado

---
+++ src/main/java/com/fatec/modelagem/ModelagemApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.modelagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelagemApplication.class, args);
+	}
+
+}
