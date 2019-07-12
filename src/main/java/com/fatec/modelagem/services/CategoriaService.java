e0e736ff41193d1c0145e728904b1e16347975c8
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2019-07-12 09:37:13 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/fatec/modelagem/services/CategoriaService.java
+++ src/main/java/com/fatec/modelagem/services/CategoriaService.java
@@ -7,15 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.fatec.modelagem.domain.Categoria;
 import com.fatec.modelagem.repositories.CategoriaRepository;
+import com.fatec.modelagem.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 	
 	@Autowired
 	private CategoriaRepository repo;
+
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-	}
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		}
 
 }
