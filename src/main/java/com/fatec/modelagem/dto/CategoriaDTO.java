6c8046740950adec6004b1e8cf0f6619b22a4a17
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2019-07-17 02:06:09 -0300

    Validação Sintática com Bean Validation

--- src/main/java/com/fatec/modelagem/dto/CategoriaDTO.java
+++ src/main/java/com/fatec/modelagem/dto/CategoriaDTO.java
@@ -2,12 +2,19 @@ package com.fatec.modelagem.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.fatec.modelagem.domain.Categoria;
 
 public class CategoriaDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=80, message="Mínimo de cinco caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {
--- src/main/java/com/fatec/modelagem/services/CategoriaService.java
+++ src/main/java/com/fatec/modelagem/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.fatec.modelagem.domain.Categoria;
+import com.fatec.modelagem.dto.CategoriaDTO;
 import com.fatec.modelagem.repositories.CategoriaRepository;
 import com.fatec.modelagem.services.exception.DataIntegrityException;
 import com.fatec.modelagem.services.exception.ObjectNotFoundException;
@@ -53,5 +54,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
