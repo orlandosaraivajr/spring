b76971a37aff73214db300dff75ace86b9679ff0
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2019-07-17 11:34:47 -0300

    PUT, DELETE e INSERT para Cliente

--- src/main/java/com/fatec/modelagem/domain/Cliente.java
+++ src/main/java/com/fatec/modelagem/domain/Cliente.java
@@ -48,7 +48,7 @@ public class Cliente  implements Serializable{
 		this.nome = nome;
 		this.email = email;
 		this.cpf_cnpj = cpf_cnpj;
-		this.tipo = tipo.getCod();
+		this.tipo = (tipo==null)? null: tipo.getCod();
 	}
 
 	public Integer getId() {
--- src/main/java/com/fatec/modelagem/services/ClienteService.java
+++ src/main/java/com/fatec/modelagem/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.fatec.modelagem.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.fatec.modelagem.domain.Cliente;
+import com.fatec.modelagem.dto.ClienteDTO;
 import com.fatec.modelagem.repositories.ClienteRepository;
+import com.fatec.modelagem.services.exception.DataIntegrityException;
 import com.fatec.modelagem.services.exception.ObjectNotFoundException;
 
 @Service
@@ -20,5 +27,36 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 		}
-
+	public Cliente update(Cliente obj) {
+		Cliente newObj = this.find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		this.find(id);
+		try {
+			this.repo.deleteById(id);			
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria com produtos");
+		}
+	}
+	
+	public List<Cliente> findAll(){
+		return this.repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
 }
