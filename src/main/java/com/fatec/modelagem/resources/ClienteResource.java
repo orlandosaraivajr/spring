f996fb64c80fc8b42742af931fc55b3d8c64465e
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2019-07-17 15:25:34 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/fatec/modelagem/resources/ClienteResource.java
+++ src/main/java/com/fatec/modelagem/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.fatec.modelagem.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,8 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import com.fatec.modelagem.domain.Cliente;
 import com.fatec.modelagem.dto.ClienteDTO;
+import com.fatec.modelagem.dto.ClienteNewDTO;
 import com.fatec.modelagem.services.ClienteService;
 
 @RestController
@@ -29,6 +33,15 @@ public class ClienteResource {
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
--- src/main/java/com/fatec/modelagem/services/ClienteService.java
+++ src/main/java/com/fatec/modelagem/services/ClienteService.java
@@ -3,6 +3,8 @@ package com.fatec.modelagem.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -10,9 +12,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.fatec.modelagem.domain.Cidade;
 import com.fatec.modelagem.domain.Cliente;
+import com.fatec.modelagem.domain.Endereco;
+import com.fatec.modelagem.domain.enums.TipoCliente;
 import com.fatec.modelagem.dto.ClienteDTO;
+import com.fatec.modelagem.dto.ClienteNewDTO;
 import com.fatec.modelagem.repositories.ClienteRepository;
+import com.fatec.modelagem.repositories.EnderecoRepository;
 import com.fatec.modelagem.services.exception.DataIntegrityException;
 import com.fatec.modelagem.services.exception.ObjectNotFoundException;
 
@@ -21,7 +28,17 @@ public class ClienteService {
 	
 	@Autowired
 	private ClienteRepository repo;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
+	
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
@@ -54,6 +71,21 @@ public class ClienteService {
 	
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2()!=null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3()!=null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
 	}
 	
 	private void updateData(Cliente newObj, Cliente obj) {
